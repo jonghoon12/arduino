73bb6917eb016e519919a8c0e56ce7c5f7855311
jonghoon12 <speed64277380@gmail.com>
2021-06-23 16:44:34 +0900

    프로젝트 끝

--- 잡것/project1st/project1st.ino
+++ 잡것/project1st/project1st.ino
@@ -1,24 +1,20 @@
-#include <Servo.h> 
+#include <Servo.h>
 #include <Wire.h>                        // i2C 통신을 위한 라이브러리
 #include <LiquidCrystal_I2C.h>        // LCD 1602 I2C용 라이브러리
-LiquidCrystal_I2C lcd(0x3F,16,2);     // 접근주소: 0x3F or 0x27
+LiquidCrystal_I2C lcd(0x27, 16, 2);   // 접근주소: 0x3F or 0x27
 
 int tmp36 = A0;
 
-Servo servo1; 
+Servo servo1;
 int servoPin1 = 9;
-int motorPin = 5;
+#define ENB 5
+#define EN3 7
+#define EN4 6
+int Motor_speed = 0;
 
 unsigned long timeVal = 0; //이전시간
 
-int segValue[4][7] = {
-//{a,b,c,d,e,f,g}
-  {1,1,1,1,1,1,0}, //0
-  {0,1,1,0,0,0,0}, //1
-  {1,1,0,1,1,0,1}, //2
-  {1,1,1,1,0,0,1}, //3
-};
-int segPin[8]={4,6,7,8,10,11,12}; //a,b,c,d,e,f,g 핀
+
 
 
 int interruptPin1 = 2; //바람 세기 스위치버튼
@@ -28,83 +24,92 @@ boolean sw_state = false; //회전스위치 상태값
 int angle = 0; //회전각
 int addAngle = 10; //회전조절각
 
-void setup() 
-{ 
-  pinMode(motorPin, OUTPUT); //DC모터
-  servo1.attach(servoPin1); //서보모터
-    
-  for(int i=0;i<7;i++){ //7-Segment Display핀 모드를 선언과 동시체 초기 0단계 표시
-    pinMode(segPin[i], OUTPUT);
-    digitalWrite(segPin[i], segValue[0][i]);
-  }
+void setup()
+{
+  Serial.begin(9600);
   
-    //인터럽트함수 선언
+  servo1.attach(servoPin1); //서보모터
+
+  //DC모터
+  pinMode(ENB, OUTPUT);
+  pinMode(EN3, OUTPUT);
+  pinMode(EN4, OUTPUT);
+
+  //인터럽트함수 선언
   pinMode(interruptPin1, INPUT_PULLUP);
   pinMode(interruptPin2, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(interruptPin1), exchange1, FALLING); 
+  attachInterrupt(digitalPinToInterrupt(interruptPin1), exchange1, FALLING);
   attachInterrupt(digitalPinToInterrupt(interruptPin2), exchange2, FALLING);
-  
+
 
   //초기 세팅
-  analogWrite(motorPin, 0); //바람세기 = 0
+  digitalWrite(EN3, HIGH);  //DC모터 방향
+  digitalWrite(EN4, LOW);
+  analogWrite(ENB, Motor_speed);  //DC모터 세기
   servo1.write(angle); //회전각 = 0
   delay(1000);
-  
-  Serial.begin(9600);  
+
+  Serial.begin(9600);
   lcd.init();                      // LCD 초기화
   lcd.backlight();                // 백 라이트 켜기
 }
 
-void loop() 
-{   
+void loop()
+{
   int sensor = analogRead(tmp36);
-      
-  float voltage = sensor * 5000.0/1024.0;     // 온도센서의 값을 전압으로 변환
+
+  float voltage = sensor * 5000.0 / 1024.0;   // 온도센서의 값을 전압으로 변환
   float celsius = (voltage - 500) / 10.0;     // 전압을 온도로 변환
-  
-  lcd.setCursor(0,0);             // 1번째, 1라인
-  lcd.print("temperature is ");
-  lcd.setCursor(6,1);             // 7번째, 2라인
+
+  lcd.setCursor(0, 0);            // 1번째, 1라인
   lcd.print(celsius);
-  lcd.setCursor(11,1);             // 12번째, 2라인
+  lcd.setCursor(5, 0);            // 6번째, 1라인
   lcd.print(" C");
+  lcd.setCursor(0, 1);            // 1번째, 2라인
+  lcd.print(step);
+  lcd.setCursor(1, 1);            // 2번째, 2라인
+  lcd.print(" step");
   delay(500);
-  if(step>0 && sw_state==true){
-    if(millis()-timeVal>=100){
-        angle=angle+addAngle;
-        if(angle==180){
-          addAngle=-10;
-        }
-        else if(angle==0){
-          addAngle=10;
-        }  
-        servo1.write(angle);        
-        timeVal = millis();
+  
+  if (step > 0 && sw_state == true) {
+    if (millis() - timeVal >= 100) {
+      angle = angle + addAngle;
+      if (angle == 180) {
+        addAngle = -10;
+      }
+      else if (angle == 0) {
+        addAngle = 10;
+      }
+      servo1.write(angle);
+      timeVal = millis();
     }
-  }   
+  }
+  analogWrite(ENB, Motor_speed);
 }
 void exchange1() {
   step++;
-  if(step==4) step=0;
-  for(int j=0;j<7;j++){
-     digitalWrite(segPin[j], segValue[step][j]);        
-  }
-  switch(step){
+  if (step == 4) step = 0;
+
+  switch (step) {
     case 0:
-     analogWrite(motorPin, 0);
-     break;
+      Motor_speed = 0;
+      Serial.println(step);
+      break;
     case 1:
-     analogWrite(motorPin, 85);
-     break;
+      Motor_speed = 85;
+      Serial.println(step);
+      break;
     case 2:
-     analogWrite(motorPin, 170);
-     break;
+      Motor_speed = 170;
+      Serial.println(step);
+      break;
     case 3:
-     analogWrite(motorPin, 255);
-     break;
+      Motor_speed = 255;
+      Serial.println(step);
+      break;
   }
 }
 void exchange2() {
-  sw_state=!sw_state;
+  sw_state = !sw_state;
   timeVal = millis();
 }
